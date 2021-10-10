72cc9d55f129a8566da114fdad8e57ce084028db
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2021-10-10 11:22:22 +0200

    Update getimagesize-solution-1.php

--- BUG-30-09-2021/getimagesize-solution-1.php
+++ BUG-30-09-2021/getimagesize-solution-1.php
@@ -6,5 +6,9 @@ $base_image_path = "";
 $image_path = $http_url.$dir_img_path."1.jpg";
 $image_path = str_replace($http_url, $base_image_path, $image_path);
 $image_path_root = $_SERVER["DOCUMENT_ROOT"].$dir_img_path."1.jpg";
-list($width, $height, $type, $attr)= getimagesize($image_path_root) 
-;?>
+list($width, $height, $type, $attr)= getimagesize($image_path_root);
+?>
+
+<?php echo "width = ".$width." height = ". $height."<br>";
+echo $_SERVER["DOCUMENT_ROOT"];
+ ?>
