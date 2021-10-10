7d4545560da6b70b20734d5086ec8138b01429bc
eurowebpage <32062729+eurowebpage@users.noreply.github.com>
2021-10-10 11:20:04 +0200

    getimagesize
    getimagesize solution 1

---
+++ BUG-30-09-2021/getimagesize-solution-1.php
@@ -0,0 +1,10 @@
+<?php
+//Images root exemple https://test.scriptphp.eu/1/edit-php-file-php/1.jpg
+$http_url ="https://test.scriptphp.eu";
+$dir_img_path = "/1/edit-php-file-php/";
+$base_image_path = "";
+$image_path = $http_url.$dir_img_path."1.jpg";
+$image_path = str_replace($http_url, $base_image_path, $image_path);
+$image_path_root = $_SERVER["DOCUMENT_ROOT"].$dir_img_path."1.jpg";
+list($width, $height, $type, $attr)= getimagesize($image_path_root) 
+;?>
